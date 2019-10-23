25411bae4d2a6d869917c63cb39edcb6d8ff4921
osamafayyaz3 <osama85@my.yorku.ca>
2019-10-23 16:32:23 -0400

    In development

---
+++ src/components/Titles.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Titles = props => (
+    <div>
+            <h1>Weather Finder</h1>
+            <p>Find weather conditions</p>
+        </div>
+)
+
+export default Titles;
